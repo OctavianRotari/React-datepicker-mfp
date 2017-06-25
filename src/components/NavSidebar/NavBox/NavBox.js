bdddf4130f6f84c6205336569b75d6c4407a1115
Octavian Rotari <octavianrotari@gmail.com>
2017-06-25 17:31:25 +0100

    style side nav (WIP)

--- src/components/NavSidebar/NavBox/NavBox.js
+++ src/components/NavSidebar/NavBox/NavBox.js
@@ -2,21 +2,22 @@ import React, { Component } from 'react';
 import { TouchableHighlight } from 'react-native';
 import _ from 'lodash';
 import { View, Text } from 'react-native';
+import styles from './styles';
 
 class NavBox extends Component {
   render() {
     const { value, name } = this.props;
     return(
       <TouchableHighlight
-        style={{flex:1}}
+        style={{minHeight: 30}}
         onPress={() => { 
           this.props.onSelectForm(value) 
         }}
         underlayColor="#048fc0"
         activeOpacity={ 0.9 }
       >
-        <View style={{flex:1}}>
-          <Text>{ name }</Text>
+        <View style={ styles.containerText }>
+          <Text style={ styles.text }>{ name }</Text>
         </View>
       </TouchableHighlight>
     )
