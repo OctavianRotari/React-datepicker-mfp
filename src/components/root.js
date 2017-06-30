5e787e1b007a48fa732045bf95010f0ee8105571
Octavian Rotari <octavianrotari@gmail.com>
2017-06-30 18:36:53 +0100

    authenticate with finger print

--- src/components/root.js
+++ src/components/root.js
@@ -5,14 +5,56 @@
  */
 
 import React, { Component } from "react";
-import { Text, View } from 'react-native';
+import { Text, View, TouchableHighlight, AlertIOS } from 'react-native';
 import { Provider } from "react-redux";
+import TouchID from 'react-native-touch-id';
+import { cmss } from '../styles/styles';
 import store from "../store/store";
 import StatusBar from './StatusBar';
 import TraumaAppContainer from '../containers/TraumaAppContainer';
 
 class Root extends Component {
+  constructor(props) {
+    super(props)
+    this.state = { authenticated: false }
+  }
+  _pressHandler() {
+    TouchID.authenticate('to demo this react-native component')
+      .then(success => {
+        AlertIOS.alert('Authenticated Successfully');
+        this.setState({authenticated: true});
+      })
+      .catch(error => {
+        AlertIOS.alert('Authentication Failed');
+        this.setState({authenticated: false});
+      });
+  }
+
   render() {
+    if(!this.state.authenticated) {
+      return (
+        <TouchableHighlight
+          style={[{
+            flex: 1,
+            flexDirection: 'row',
+            justifyContent: 'center',
+            alignItems: 'center'
+          }, cmss.bgClr ]}
+          onPress={this._pressHandler()}>
+          <View
+            style={[{
+              flex: 1,
+              flexDirection: 'row',
+              justifyContent: 'center',
+              alignItems: 'center'
+            }, cmss.bgClr ]}
+          >
+            <StatusBar style={{flex:1}}/>
+            <Text style={{flex:1, textAlign:'center'}}>Authenticate with Touch ID</Text>
+          </View>
+        </TouchableHighlight>
+      )
+    }
     return (
       <Provider store={store}>
         <View style={{flex:1}}>
