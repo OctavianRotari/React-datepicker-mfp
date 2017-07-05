b7acf2644e06af4d9b4c940cc074ee22ec9b0440
Octavian Rotari <octavianrotari@gmail.com>
2017-07-05 12:40:47 +0100

    authenticate with fingerprint

--- src/components/root.js
+++ src/components/root.js
@@ -19,17 +19,40 @@ class Root extends Component {
     this.state = { authenticated: false }
   }
   _pressHandler() {
-    TouchID.authenticate('Authenticate before proceeding')
+    TouchID.authenticate('Authenticate to proceed.')
       .then(success => {
-        AlertIOS.alert('Authenticated Successfully');
         this.setState({authenticated: true});
       })
       .catch(error => {
-        AlertIOS.alert('Authentication Failed');
         this.setState({authenticated: false});
       });
   }
+
   render() {
+    if(!this.state.authenticated) {
+      return (
+        <TouchableHighlight
+          style={[{
+            flex: 1,
+            flexDirection: 'row',
+            justifyContent: 'center',
+            alignItems: 'center'
+          }, cmss.bgClr ]}
+          onPress={this._pressHandler()}>
+          <View
+            style={[{
+              flex: 1,
+              flexDirection: 'row',
+              justifyContent: 'center',
+              alignItems: 'center'
+            }, cmss.bgClr ]}
+          >
+            <StatusBar style={{flex:1}}/>
+            <Text style={{flex:1, textAlign:'center'}}>Authenticate with Touch ID</Text>
+          </View>
+        </TouchableHighlight>
+      )
+    }
     return (
       <Provider store={store}>
         <View style={{flex:1}}>
