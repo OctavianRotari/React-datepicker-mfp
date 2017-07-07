5aaed6b197c96c90ec2f0bafcfaa1f181cd5aba9
Octavian Rotari <octavianrotari@gmail.com>
2017-07-07 10:09:20 +0100

    composers return null if no event queue

--- src/containers/TraumaAppContainer.js
+++ src/containers/TraumaAppContainer.js
@@ -3,19 +3,19 @@ import { connect } from 'react-redux';
 import TraumaApp from '../components/TraumaApp';
 import { getAppData } from '../actions/getAppData';
 
-function mapStateToProps(state) {
-  return {
-    appData: state.appData,
-    activeForm: state.activeForm
-  }
-}
-
 function mapDispatchToProps(dispatch) {
   return {
     onLoaded: () => {
       dispatch(getAppData())
     }
   };
+}
+
+function mapStateToProps(state) {
+  return {
+    appData: state.appData,
+    activeForm: state.activeForm
+  }
 }
 
 const TraumaAppContainer = connect(mapStateToProps, mapDispatchToProps)(TraumaApp);
