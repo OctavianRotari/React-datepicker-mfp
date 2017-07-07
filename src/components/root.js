87c610da2508413ebc2cd061cef2854c46c70d33
Octavian Rotari <octavianrotari@gmail.com>
2017-07-07 14:01:48 +0100

    add module for fingerprint auth

--- src/components/root.js
+++ src/components/root.js
@@ -7,13 +7,24 @@
 import React, { Component } from "react";
 import { Text, View, TouchableHighlight, AlertIOS } from 'react-native';
 import { Provider } from "react-redux";
-import TouchID from 'react-native-touch-id';
+import FingerprintScanner from 'react-native-fingerprint-scanner';
 import { cmss } from '../styles/styles';
 import store from "../store/store";
 import StatusBar from './StatusBar';
 import TraumaApp from '../components/TraumaApp';
 
 class Root extends Component {
+  componentDidMount() {
+    FingerprintScanner
+      .authenticate({ description: 'Scan your fingerprint on the device scanner to continue' })
+      .then(() => {
+        AlertIOS.alert('Authenticated successfully');
+      })
+      .catch((error) => {
+        AlertIOS.alert(error.message);
+      });
+  }
+
   render() {
     return (
       <Provider store={store}>
